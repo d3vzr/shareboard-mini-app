016d07fa7e4f343d3e81903b5900f091371fbaed
Ahmed Lo'ay <geniero.bas@mgail.com>
2017-08-24 01:46:35 +0200

    views

---
+++ views/main.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Shareboard</title>
+
+    <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+  <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Shareboard</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="<?php echo ROOT_URL; ?>">Home</a></li>
+            <li><a href="<?php echo ROOT_URL; ?>shares">Shares</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+          <?php if (isset($_SESSION['is_logged_in'])): ?>
+            <li><a href="<?php echo ROOT_URL; ?>">Welcome, <?php echo $_SESSION['user_data']['name']; ?></a></li>
+            <li><a href="<?php echo ROOT_URL; ?>users/logout">Logout</a></li>
+          <?php else: ?>
+            <li><a href="<?php echo ROOT_URL; ?>users/login">Login</a></li>
+            <li><a href="<?php echo ROOT_URL; ?>users/register">Register</a></li>
+          <?php endif; ?>
+          </ul>
+        </div><!--/.nav-collapse -->
+
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="row">
+      <?php Messages::display(); ?>
+        <?php require($view) ?>
+      </div>
+    </div><!-- /.container -->
+
+    <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
