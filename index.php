b3fc703def6cd1508f588fe23eafb6a60b801c8a
Ahmed Lo'ay <geniero.bas@mgail.com>
2017-08-24 01:43:55 +0200

    session_start(), require models and Messages

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <?php
 
-// include config
+// start session
+session_start();
 
+// include config
 require('config.php');
 
+// include classes
+require('classes/Messages.php');
 require('classes/Bootstrap.php');
 require('classes/Controller.php');
+require('classes/Model.php');
 
+// include controllers
 require('controllers/home.php');
 require('controllers/shares.php');
 require('controllers/users.php');
+
+// include models
+require('models/home.php');
+require('models/share.php');
+require('models/user.php');
 
 $bootstrap = new Bootstrap($_GET);
 $controller = $bootstrap->createController();
