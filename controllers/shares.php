04a5330245b0c01d28c18a9f9169570acbed0a0a
Ahmed Lo'ay <geniero.bas@mgail.com>
2017-08-24 01:42:12 +0200

    index() to show shares, and add() to add new ones

--- controllers/shares.php
+++ controllers/shares.php
@@ -6,8 +6,21 @@
 class Shares extends Controller
 {
   
-  protected function Index()
+  // the function that is called when the controller is instantiated
+  // from the second 'this' inside the executeAction()
+  // because the default action is 'Index'
+  protected function index()
   {
-    echo 'SHARES/index';
+    $viewmodel = new ShareModel();
+    $this->returnView($viewmodel->index(), true);
+  }
+
+  protected function add()
+  {
+    if (!isset($_SESSION['is_logged_in'])) {
+      header('Location: ' . ROOT_URL . 'shares');
+    }
+    $viewmodel = new ShareModel();
+    $this->returnView($viewmodel->add(), true);
   }
 }
